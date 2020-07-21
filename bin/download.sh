fff4a41148182a358778d37f579eca7bfd415a4c
John David Garza <john.garza@utsa.edu>
2020-07-21 10:55:26 -0500

    adding more models for testing, adding run script

--- bin/download.sh
+++ bin/download.sh
@@ -4,7 +4,8 @@ model_dir="./models/"
 
 omz_dl="/opt/intel/openvino/deployment_tools/open_model_zoo/tools/downloader/downloader.py"
 
-face_detection="face-detection-adas-binary-0001"
+face_detection_bin="face-detection-adas-binary-0001"
+face_detection="face-detection-adas-0001"
 head_position="head-pose-estimation-adas-0001"
 land_marks="landmarks-regression-retail-0009"
 gaze_estimation="gaze-estimation-adas-0002"
@@ -12,6 +13,7 @@ gaze_estimation="gaze-estimation-adas-0002"
 mkdir ${model_dir}
 
 #download our models
+python3 ${omz_dl} --name ${face_detection_bin} -o ${model_dir}
 python3 ${omz_dl} --name ${face_detection} -o ${model_dir}
 python3 ${omz_dl} --name ${head_position} -o ${model_dir}
 python3 ${omz_dl} --name ${land_marks} -o ${model_dir}
